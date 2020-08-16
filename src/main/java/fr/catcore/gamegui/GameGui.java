f3878e52695ec549444a01bcfd75327e95190c9a
CatCore <arthur.bambou@gmail.com>
2020-08-16 20:48:20 +0200

    add suggested GameType icons

--- src/main/java/fr/catcore/gamegui/GameGui.java
+++ src/main/java/fr/catcore/gamegui/GameGui.java
@@ -34,6 +34,15 @@ public class GameGui implements ModInitializer {
         map.put("farmy_feud:farmy_feud", Items.WHITE_WOOL);
         map.put("colorswap:color_swap", Items.JUKEBOX);
         map.put("electricfloor:electric_floor", Items.WHITE_STAINED_GLASS);
+        map.put("ascension:ascension", Items.RABBIT_FOOT);
+        map.put("withersweeper:withersweeper", Items.SOUL_SOIL);
+        map.put("shardthief:shard_thief", Items.PRISMARINE_SHARD);
+        map.put("murder_mystery:murder_mystery", Items.STONE_SWORD);
+        map.put("wavedefense:wavedefense", Items.ZOMBIE_HEAD);
+        map.put("koth:koth", Items.LADDER);
+        map.put("clutchbridge:clutch_bridge", Items.MAGENTA_GLAZED_TERRACOTTA);
+        map.put("territorybattle:territory_battle", Items.GREEN_WOOL);
+        map.put("castlewars:castlewars", Items.VILLAGER_SPAWN_EGG);
 
         gameTypeItemConvertible = new ImmutableMap.Builder<String, ItemConvertible>().putAll(map).build();
     }
