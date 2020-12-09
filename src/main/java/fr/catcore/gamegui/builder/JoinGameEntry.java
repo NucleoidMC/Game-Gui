ecb0f8153429c8280d67086b1a7b8e6538c8a4c0
gegy1000 <gegy1000@gmail.com>
2020-12-09 15:25:29 +0200

    use game config name as title for join game item

--- src/main/java/fr/catcore/gamegui/builder/JoinGameEntry.java
+++ src/main/java/fr/catcore/gamegui/builder/JoinGameEntry.java
@@ -2,30 +2,25 @@ package fr.catcore.gamegui.builder;
 
 import fr.catcore.gamegui.GameGui;
 import fr.catcore.gamegui.item.JoinGameItem;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.MessageType;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import xyz.nucleoid.plasmid.game.ConfiguredGame;
 import xyz.nucleoid.plasmid.game.GameType;
 import xyz.nucleoid.plasmid.game.ManagedGameSpace;
 import xyz.nucleoid.plasmid.game.config.GameConfigs;
 import xyz.nucleoid.plasmid.util.ItemStackBuilder;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.ItemStack;
-import net.minecraft.network.MessageType;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.Util;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
 
 public class JoinGameEntry {
     private final ItemStack icon;
     private Identifier gameConfigId;
-    private RegistryKey<World> worldRegistryKey;
+    private ManagedGameSpace gameSpace;
 
     private JoinGameEntry(ItemStack icon) {
         this.icon = icon;
@@ -44,57 +39,46 @@ public class JoinGameEntry {
         return this;
     }
 
-    public JoinGameEntry withDimensionKey(RegistryKey<World> worldRegistryKey) {
-        this.worldRegistryKey = worldRegistryKey;
+    public JoinGameEntry withGameSpace(ManagedGameSpace gameSpace) {
+        this.gameSpace = gameSpace;
         return this;
     }
 
     public ItemStack createIcon(ServerPlayerEntity player) {
         ConfiguredGame<?> configuredGame = GameConfigs.get(this.gameConfigId);
         GameType<?> gameType = configuredGame.getType();
+
+        TranslatableText typeName = new TranslatableText("text.game_gui.join.type", GameGui.getGameTypeName(gameType.getIdentifier()));
+        TranslatableText configName = new TranslatableText("text.game_gui.join.config", GameGui.getGameConfigName(this.gameConfigId));
+
         ItemStackBuilder iconBuilder = ItemStackBuilder.of(this.icon);
-        iconBuilder.addLore(new TranslatableText("text.game_gui.join.config", GameGui.getGameConfigName(this.gameConfigId)));
-        iconBuilder.addLore(new TranslatableText("text.game_gui.join.type", GameGui.getGameTypeName(gameType.getIdentifier())));
+        iconBuilder.setName(configName);
+        iconBuilder.addLore(typeName);
 
-        MinecraftServer minecraftServer = player.getServer();
-        ServerWorld serverWorld = minecraftServer.getWorld(this.worldRegistryKey);
-        if (serverWorld == null) {
+        ManagedGameSpace gameSpace = this.gameSpace;
+        if (gameSpace == null) {
             player.closeHandledScreen();
             JoinGameItem.openJoinScreen(player);
+            return ItemStack.EMPTY;
         }
-        ManagedGameSpace openWorld = ManagedGameSpace.forWorld(serverWorld);
-        if (openWorld == null) {
-            player.closeHandledScreen();
-            JoinGameItem.openJoinScreen(player);
-        }
-        int playerCount = 0;
-        try {
-            playerCount = openWorld.getPlayerCount();
-        } catch (NullPointerException e) {}
-        iconBuilder.addLore(new TranslatableText("text.game_gui.join.player_count", playerCount));
 
-        ItemStack icon = iconBuilder.build().copy();
-        icon.setCustomName(new LiteralText(this.worldRegistryKey.getValue().toString()));
-        return icon;
+        iconBuilder.addLore(new TranslatableText("text.game_gui.join.player_count", gameSpace.getPlayers()));
+
+        return iconBuilder.build();
     }
 
     public void onClick(ServerPlayerEntity player) {
-        MinecraftServer minecraftServer = player.getServer();
-        ServerWorld serverWorld = minecraftServer.getWorld(this.worldRegistryKey);
-        if (serverWorld == null) {
-            throw new NullPointerException();
-        }
-        ManagedGameSpace openWorld = ManagedGameSpace.forWorld(serverWorld);
-        if (openWorld == null) {
+        ManagedGameSpace gameSpace = this.gameSpace;
+        if (gameSpace == null) {
             player.closeHandledScreen();
-        } else {
-            openWorld.offerPlayer(player).thenAccept((joinResult) -> {
-                if (joinResult.isError()) {
-                    Text error = joinResult.getError();
-                    player.sendMessage(error.shallowCopy().formatted(Formatting.RED), MessageType.CHAT, Util.NIL_UUID);
-                }
-
-            });
+            return;
         }
+
+        gameSpace.offerPlayer(player).thenAccept((joinResult) -> {
+            if (joinResult.isError()) {
+                Text error = joinResult.getError();
+                player.sendMessage(error.shallowCopy().formatted(Formatting.RED), MessageType.CHAT, Util.NIL_UUID);
+            }
+        });
     }
 }
--- src/main/java/fr/catcore/gamegui/item/JoinGameItem.java
+++ src/main/java/fr/catcore/gamegui/item/JoinGameItem.java
@@ -3,11 +3,11 @@ package fr.catcore.gamegui.item;
 import fr.catcore.gamegui.GameGui;
 import fr.catcore.gamegui.builder.JoinGameEntry;
 import fr.catcore.gamegui.ui.JoinGameUi;
+import fr.catcore.gamegui.util.GameInfos;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
@@ -43,10 +43,8 @@ public final class JoinGameItem extends Item implements FakeItem {
 
     public static void openJoinScreen(PlayerEntity playerEntity) {
         playerEntity.openHandledScreen(JoinGameUi.create(new TranslatableText("text.game_gui.gui.join"), joinGameBuilder -> {
-            for (ServerWorld serverWorld : playerEntity.getServer().getWorlds()) {
-                ManagedGameSpace gameWorld = ManagedGameSpace.forWorld(serverWorld);
-                if (gameWorld == null) continue;
-                ConfiguredGame<?> configuredGame = gameWorld.getGameConfig();
+            for (ManagedGameSpace gameSpace : ManagedGameSpace.getOpen()) {
+                ConfiguredGame<?> configuredGame = gameSpace.getGameConfig();
                 Identifier gameID = new Identifier("game-gui", "null");
                 for (Identifier id : GameConfigs.getKeys()) {
                     if (GameConfigs.get(id) == configuredGame) {
@@ -54,7 +52,13 @@ public final class JoinGameItem extends Item implements FakeItem {
                         break;
                     }
                 }
-                joinGameBuilder.add(JoinGameEntry.ofItem(GameGui.getGameInfos(configuredGame.getType().getIdentifier()).getIcon()).withDimensionKey(serverWorld.getRegistryKey()).withGameConfig(gameID));
+
+                GameInfos gameInfo = GameGui.getGameInfos(configuredGame.getType().getIdentifier());
+                joinGameBuilder.add(
+                        JoinGameEntry.ofItem(gameInfo.getIcon())
+                                .withGameSpace(gameSpace)
+                                .withGameConfig(gameID)
+                );
             }
         }));
     }
