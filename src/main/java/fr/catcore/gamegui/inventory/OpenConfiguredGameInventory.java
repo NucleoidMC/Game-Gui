eb8b277315916909def0b9a0b53a838142e32787
Cat Core <arthur.bambou@gmail.com>
2021-01-20 16:03:14 +0100

    Why are there so many nucleoid:random configs?

--- src/main/java/fr/catcore/gamegui/inventory/OpenConfiguredGameInventory.java
+++ src/main/java/fr/catcore/gamegui/inventory/OpenConfiguredGameInventory.java
@@ -5,9 +5,9 @@ import fr.catcore.gamegui.builder.OpenConfiguredGameEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
 import java.util.function.Consumer;
 
-public class OpenConfiguredGameInventory extends GameGUIInventory<OpenConfiguredGameBuilder, OpenConfiguredGameEntry> {
+public class OpenConfiguredGameInventory extends PagedGameGUIInventory<OpenConfiguredGameBuilder> {
 
     public OpenConfiguredGameInventory(ServerPlayerEntity player, Consumer<OpenConfiguredGameBuilder> builder) {
-        super(player, builder);
+        super(player, builder, 0);
     }
 }
